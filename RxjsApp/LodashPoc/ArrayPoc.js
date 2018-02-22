ee8d66fcaadc8c47f9ab2addba5780c259571196
He Ke Henry Zhang <He.Ke.Henry.Zhang@dnvgl.com>
2018-02-22 12:24:39 +0800

    lodash drop

--- RxjsApp/LodashPoc/ArrayPoc.js
+++ RxjsApp/LodashPoc/ArrayPoc.js
@@ -3,12 +3,52 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const _ = require("lodash");
 class ArrayPoc {
     test() {
-        this.func1();
+        // this.chunkFun();
+        // this.compactFun();
+        // this.concatFun();
+        this.dropFun();
     }
-    func1() {
+    // tslint:disable-next-line:max-line-length
+    // Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
+    chunkFun() {
         const original = ["a", "b", "c", "d"];
         const target = _.chunk(original, 2);
         _.forEach(target, (val) => console.log(val));
+    }
+    /**
+     * Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+     */
+    compactFun() {
+        const target = _.compact([0, 1, false, 2, "", 3]);
+        _.forEach(target, (val) => console.log(val));
+    }
+    /**
+     * Creates a new array concatenating array with any additional arrays and/or values.
+     */
+    concatFun() {
+        const array = [1];
+        // const other =  _.concat(array,  2,  [3],  [[4]]);
+        const other = _.concat(array, 2, [3], [4]);
+        console.log(other);
+        console.log(array);
+    }
+    /**
+     * Creates a slice of array with n elements dropped from the beginning.
+     */
+    dropFun() {
+        const array = _.drop([1, 2, 3], 2);
+        console.log(array);
+        const array1 = _.dropRight([1, 2, 3], 2);
+        console.log(array1);
+        const users = [
+            { user: "barney", active: true },
+            { user: "fred", active: false },
+            { user: "pebbles", active: false }
+        ];
+        const v1 = _.dropRightWhile(users, (o) => !o.active);
+        console.log(v1);
+        const v2 = _.dropWhile(users, (o) => !o.active);
+        console.log(v2);
     }
 }
 exports.ArrayPoc = ArrayPoc;
--- RxjsApp/LodashPoc/ArrayPoc.ts
+++ RxjsApp/LodashPoc/ArrayPoc.ts
@@ -2,12 +2,58 @@ import * as _ from "lodash";
 
 export class ArrayPoc {
     public test() {
-        this.func1();
+        // this.chunkFun();
+        // this.compactFun();
+        // this.concatFun();
+        this.dropFun();
     }
 
-    public func1() {
+    // tslint:disable-next-line:max-line-length
+    // Creates an array of elements split into groups the length of size. If array can't be split evenly, the final chunk will be the remaining elements.
+    public chunkFun() {
         const original = ["a",  "b",  "c",  "d"];
         const target = _.chunk(original, 2);
         _.forEach(target, (val) => console.log(val));
+    }
+
+    /**
+     * Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+     */
+    public compactFun() {
+        const target = _.compact([0,  1,  false,  2,  "",  3]);
+        _.forEach(target, (val) => console.log(val));
+    }
+
+    /**
+     * Creates a new array concatenating array with any additional arrays and/or values.
+     */
+    public concatFun() {
+        const array =  [1];
+        // const other =  _.concat(array,  2,  [3],  [[4]]);
+        const other =  _.concat(array,  2,  [3],  [4]);
+        console.log(other);
+        console.log(array);
+    }
+
+    /**
+     * Creates a slice of array with n elements dropped from the beginning.
+     */
+    public dropFun() {
+        const array = _.drop([1,  2,  3],  2);
+        console.log(array);
+
+        const array1 = _.dropRight([1,  2,  3],  2);
+        console.log(array1);
+
+        const users =  [
+              { user:  "barney",   active:  true },
+              { user:  "fred",     active:  false },
+              { user:  "pebbles",  active:  false }];
+
+        const v1 = _.dropRightWhile(users,  (o) => !o.active);
+        console.log(v1);
+
+        const v2 = _.dropWhile(users,  (o) => !o.active);
+        console.log(v2);
     }
 }
