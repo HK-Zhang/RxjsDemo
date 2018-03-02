df2560b1c48a4fd1f2e4ca78998e347dad519ebe
yxzhk <yxzhk@hotmail.com>
2018-03-02 21:00:17 +0800

    conditional

--- RxjsApp/Conditional/rxjsDefaultIfEmpty.js
+++ RxjsApp/Conditional/rxjsDefaultIfEmpty.js
@@ -1,28 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Observable_1 = require("rxjs/Observable");
-require("rxjs/add/operator/defaultIfEmpty");
 require("rxjs/add/observable/of");
-//import 'rxjs/add/observable/timer';
+require("rxjs/add/operator/defaultIfEmpty");
+const Observable_1 = require("rxjs/Observable");
 class DefaultIfEmptyPoc {
     test() {
         this.func1();
-        //this.func2();
+        // this.func2();
     }
     func1() {
         const empty = Observable_1.Observable.of();
-        //emit 'Observable.of() Empty!' when empty, else any values from source
-        const exampleOne = empty.defaultIfEmpty('Observable.of() Empty!');
-        //output: 'Observable.of() Empty!'
-        const subscribe = exampleOne.subscribe(val => console.log(val));
+        // emit 'Observable.of() Empty!' when empty, else any values from source
+        const exampleOne = empty.defaultIfEmpty("Observable.of() Empty!");
+        // output: 'Observable.of() Empty!'
+        const subscribe = exampleOne.subscribe((val) => console.log(val));
     }
     func2() {
-        //empty observable
+        // empty observable
         const empty = Observable_1.Observable.empty();
-        //emit 'Observable.empty()!' when empty, else any values from source
-        const example = empty.defaultIfEmpty('Observable.empty()!');
-        //output: 'Observable.empty()!'
-        const subscribe = example.subscribe(val => console.log(val));
+        // emit 'Observable.empty()!' when empty, else any values from source
+        const example = empty.defaultIfEmpty("Observable.empty()!");
+        // output: 'Observable.empty()!'
+        const subscribe = example.subscribe((val) => console.log(val));
     }
 }
 exports.DefaultIfEmptyPoc = DefaultIfEmptyPoc;
